f31a3d27e854492bd2e40381c6cc1cb94ec3d998
Tạ Quang Khôi <khoi0941@gmail.com>
2023-06-12 16:15:31 +0700

    🔥 refactor(main.dart): remove unused variable wordPair The variable wordPair was not being used in the build method of the _RandomWordsState class. Therefore, it was removed to improve code readability and maintainability.

--- First-Project/lib/main.dart
+++ First-Project/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
       home: RandomWords(),
     );
@@ -30,7 +30,6 @@ class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
 
-    final wordPair = WordPair.random();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Welcome to Flutter'),
