8a82166adbc9fefc7a1dd9234d0fa2a89654b748
Tạ Quang Khôi <khoi0941@gmail.com>
2023-06-17 09:59:45 +0700

    🚀 chore(main.dart): refactor MyApp to be a StatefulWidget and add button functionality The MyApp widget has been refactored to be a StatefulWidget to allow for the addition of button functionality. The button functionality has been added to the app's body, which now contains a Column widget with a Text widget and an ElevatedButton widget. The Text widget displays the text variable, which is updated by the button's onPressed function. The MyApp widget's state is managed by the _MyAppState class, which has an initState function that sets the initial value of the text variable. The btnTap function updates the text variable when the button is pressed using the setState function.

--- flutterhelloworld/lib/main.dart
+++ flutterhelloworld/lib/main.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MyApp()
-  );
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String text = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    text = 'initState: Hello World';
+    super.initState();
+  }
+
+  void btnTap() {
+    setState(() {
+      text = 'setState: Hi Everyone';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hello World App",
+      title: 'Hello World App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Hello World App"),
+          title: const Text('Hello App'),
         ),
-        body: const Center(
-          child: Text("Hello World"),
-        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text),
+            ElevatedButton(onPressed: btnTap, child: const Text('Tap Me'))
+          ],
+        )),
       ),
     );
   }
