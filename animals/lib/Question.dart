3eb4d1e072251ca653f30fa0a27f1b2f96c6f7d7
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-01 09:43:34 +0700

    🔀 refactor(Question.dart): add rightAnswer property to the Question class for better encapsulation and readability The rightAnswer property is added to the Question class to store the index of the correct answer. This change improves the encapsulation of the Question class by keeping all the necessary information related to a question in one place. It also enhances the readability of the code by making it clear which answer is the correct one.

--- animals/lib/Question.dart
+++ animals/lib/Question.dart
@@ -6,18 +6,22 @@ class Question {
   String answer3 = '';
   String answer4 = '';
 
+  int rightAnswer = 0;
+
   Question(
       {required String q,
       required String i,
       required String a,
       required String b,
       required String c,
-      required String d}) {
+      required String d,
+      required int r}) {
     questionText = q;
     imageUrl = i;
     answer1 = a;
     answer2 = b;
     answer3 = c;
     answer4 = d;
+    rightAnswer = r;
   }
 }
