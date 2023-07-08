d7b041a519e0f8ad6e139c38c92d9b8acfeddb92
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-08 07:42:20 +0700

    Create FirstScreen stless widget

--- learn_navigation/lib/main.dart
+++ learn_navigation/lib/main.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+     title: 'Route',
+      home: FirstScreen(),
+    )
+  );
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+        backgroundColor: Colors.teal[300],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Second Screen', style: TextStyle(fontSize: 30, color: Colors.black),),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.orange[300],
+          ),
+          onPressed: () {
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(builder: (context) => const SecondScreen()),
+            // );
+          },
+        ),
+      ),
+    );
+  }
 }
