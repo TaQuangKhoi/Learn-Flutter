f6bb754a0767b6aecd29b10ac47d7841a24c9335
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-08 07:44:06 +0700

    Create stless SecondScreen

--- learn_navigation/lib/main.dart
+++ learn_navigation/lib/main.dart
@@ -36,3 +36,29 @@ class FirstScreen extends StatelessWidget {
     );
   }
 }
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+        backgroundColor: Colors.orange[300],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('First Screen', style: TextStyle(fontSize: 30, color: Colors.black),),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.teal,
+          ),
+          onPressed: () {
+            // Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
+
