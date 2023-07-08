21f66e1157d3ab1c8dc371befe0fec29b80b0ff7
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-08 09:51:37 +0700

    ✨ feat(ByNumber.dart): add new widget ByNumber to display fortune telling results based on a number The ByNumber.dart file is a new addition to the project. It introduces a new widget called ByNumber, which is a StatefulWidget. This widget is responsible for displaying fortune telling results based on a number. The widget currently returns a Placeholder widget, which will be replaced with the actual implementation of the fortune telling logic.

---
+++ love_fortune_telling/lib/ByNumber.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ByNumber extends StatefulWidget {
+  const ByNumber({super.key});
+
+  @override
+  State<ByNumber> createState() => _ByNumberState();
+}
+
+class _ByNumberState extends State<ByNumber> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
