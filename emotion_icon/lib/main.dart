5fc036d7b2c1fa9bd59981007e2eebddd8b0e1d6
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-10 15:29:38 +0700

    🔀 refactor(main.dart): replace _counter variable with emojis list to improve code readability and maintainability The _counter variable has been replaced with a list of Emoji objects called emojis. This change improves code readability and maintainability by using a more descriptive and meaningful variable name. The emojis list now contains Emoji objects representing different emotions.

--- emotion_icon/lib/main.dart
+++ emotion_icon/lib/main.dart
@@ -40,18 +40,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var emojis = <Emoji>[
+    Emoji('coffee', '☕'),
+    Emoji('heart', '❤️'),
+    Emoji('party', '🎉'),
+    Emoji('sun', '☀️'),
+    Emoji('moon', '🌙'),
+    Emoji('star', '⭐'),
+    Emoji('cloud', '☁️'),
+  ];
 
   @override
   Widget build(BuildContext context) {
