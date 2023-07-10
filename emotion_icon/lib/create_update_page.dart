a4f90c805684259fbeef45c9cd7e3bf112e9b408
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-10 15:48:17 +0700

    Create stful CreateUpdateEmojiPage

---
+++ emotion_icon/lib/create_update_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateUpdateEmojiPage extends StatefulWidget {
+  const CreateUpdateEmojiPage({super.key});
+
+  @override
+  State<CreateUpdateEmojiPage> createState() => _CreateUpdateEmojiPageState();
+}
+
+class _CreateUpdateEmojiPageState extends State<CreateUpdateEmojiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
