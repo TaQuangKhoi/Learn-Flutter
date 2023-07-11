a1b2e2313d40fc70c75376efab5dc8d04dbab4a8
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-11 21:29:12 +0700

    Format code, put row to expanded widget

--- emotion_icon/lib/item_icon.dart
+++ emotion_icon/lib/item_icon.dart
@@ -15,55 +15,61 @@ class ItemIcon extends StatelessWidget {
       required this.emoji,
       required this.removeEmoji,
       required this.addEmoji,
-      required this.updateEmoji, required this.index});
+      required this.updateEmoji,
+      required this.index});
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              emoji.name,
-              style: const TextStyle(
-                fontSize: 30,
-                fontFamily: 'Agdasima',
+        Expanded(
+          flex: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                emoji.name,
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Agdasima',
+                ),
               ),
-            ),
-            Text(
-              emoji.code,
-              style: const TextStyle(
-                fontSize: 30,
-                fontFamily: 'Twemoji',
+              Text(
+                emoji.code,
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Twemoji',
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CreateUpdateEmojiPage(
-                        mode: 'update',
-                        emojiNameToUpdate: emoji.name,
-                      ),
-                    ),
-                  );
-                },
-                child: const Icon(Icons.edit)),
-            ElevatedButton(
-                onPressed: () {
-                  removeEmoji(emoji.name);
-                },
-                child: const Icon(Icons.delete)),
-          ],
-        ),
+        Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CreateUpdateEmojiPage(
+                            mode: 'update',
+                            emojiNameToUpdate: emoji.name,
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Icon(Icons.edit)),
+                ElevatedButton(
+                    onPressed: () {
+                      removeEmoji(emoji.name);
+                    },
+                    child: const Icon(Icons.delete)),
+              ],
+            )),
       ],
     );
   }
--- emotion_icon/lib/main.dart
+++ emotion_icon/lib/main.dart
@@ -100,8 +100,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
           child: ListView.builder(
               itemCount: emojis.length,
               itemBuilder: (context, index) {
